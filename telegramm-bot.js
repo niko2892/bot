78b93bae42648c69ba395d78b122444fbf3ac2d7
root <root@studygroup.site>
2024-06-17 21:25:48 +0300

    -

--- telegramm-bot.js
+++ telegramm-bot.js
@@ -2,8 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const now = new Date();
+const dotenvFilePath = '/root/bot/.env';
 
-dotenv.config();
+dotenv.config({path: dotenvFilePath});
 
 let isEnabled = 0;
 
@@ -38,12 +39,12 @@ bot.on('text', async msg => {
         if (msg.text == '/start') {
             isEnabled = 1;
             // Изменяем значение переменной
-            const newEnvContent = fs.readFileSync('.env', 'utf-8')
+            const newEnvContent = fs.readFileSync(dotenvFilePath, 'utf-8')
                 .replace('IS_BOT_ENABLED=false', 'IS_BOT_ENABLED=true');
             // Записываем изменения в файл .env
-            fs.writeFileSync('.env', newEnvContent);
+            fs.writeFileSync(dotenvFilePath, newEnvContent);
             // Перезагружаем переменные из .env
-            dotenv.config({ path: '.env' }); // Перезагружаем dotenv
+            dotenv.config({ path: dotenvFilePath }); // Перезагружаем dotenv
             await bot.sendMessage(msg.chat.id, `Вы запустили бота!`);
             console.log(`${now} : Бот включен. Значение переменной IS_BOT_ENABLED изменено на 1`);
         } else if (msg.text == '/status') {
@@ -54,10 +55,10 @@ bot.on('text', async msg => {
             }
         } else if (msg.text == '/stop') {
             isEnabled = 0;
-            const newEnvContent = fs.readFileSync('.env', 'utf-8')
+            const newEnvContent = fs.readFileSync(dotenvFilePath, 'utf-8')
                 .replace('IS_BOT_ENABLED=true', 'IS_BOT_ENABLED=false');
-            fs.writeFileSync('.env', newEnvContent);
-            dotenv.config({ path: '.env' }); // Перезагружаем dotenv
+            fs.writeFileSync(dotenvFilePath, newEnvContent);
+            dotenv.config({ path: dotenvFilePath }); // Перезагружаем dotenv
             await bot.sendMessage(msg.chat.id, `Вы выключили бота!`);
             console.log(`${now} : Бот выключен. Значение переменной IS_BOT_ENABLED изменено на 0`);
         } else {
